a136926c87a086008a62383dadc67cd4c8621f12
vitaly <optinsoft@gmail.com>
2024-10-22 11:25:09 +0500

    save images to folder

--- CapSolverProxy/CapSolverService.cs
+++ CapSolverProxy/CapSolverService.cs
@@ -5,10 +5,9 @@
     using System.Net.Http.Json;
     using System.Security.Cryptography;
     using System.Text;
-    using Microsoft.AspNetCore.WebUtilities;
     using Microsoft.Extensions.Caching.Memory;
     using Newtonsoft.Json;
-
+    
     public class CapSolverService
     {
         private readonly HttpClient client;
@@ -16,6 +15,8 @@
         private readonly MemoryCache cache;
         private readonly ILogger? logger;
         private readonly CapSolverStats stats;
+        private readonly string ImagesFolder;
+        private readonly bool SaveImages;
 
         public CapSolverService(CapSolverSettings settings, ILoggerFactory? loggerFactory) {
             client = new();
@@ -30,6 +31,12 @@
             });
             logger = loggerFactory?.CreateLogger("CapSolverService");
             stats = new CapSolverStats();
+            ImagesFolder = settings.ImagesFolder ?? "";
+            if (ImagesFolder.Length > 0 && !ImagesFolder.EndsWith("/") && !ImagesFolder.EndsWith("\\"))
+            {
+                ImagesFolder += Path.DirectorySeparatorChar;
+            }
+            SaveImages = settings.SaveImages;
         }
 
         public static string? GetImagesHash(CreateTaskRequest? request)
@@ -56,6 +63,23 @@
             return imageHash;
         }
 
+        private static void SaveImagesToFolder(CreateTaskRequest? request, string imagesFolder, string imagesHash)
+        {
+            if (request?.task?.images?.Count > 0)
+            {
+                int imageNum = 0;
+                foreach (var image in request.task.images)
+                {
+                    var imageBytes = Convert.FromBase64String(image);
+                    string fileName = string.Format("{0}{1}_{2}.jpg", imagesFolder, imagesHash, imageNum);
+                    using (var fs = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+                    {
+                        fs.Write(imageBytes, 0, imageBytes.Length);
+                    }
+                }
+            }
+        }
+
         public async Task<string> CreateTask(string requestJson)
         {
             stats.IncRequests();
@@ -70,6 +94,10 @@
                         stats.IncFromCache();
                         logger?.LogInformation("Response from cache for {}", imagesHash);
                         return responseJson;
+                    }
+                    if (SaveImages && ImagesFolder.Length > 0)
+                    {
+                        SaveImagesToFolder(request, ImagesFolder, imagesHash);
                     }
                 }
                 HttpResponseMessage response = await client.PostAsJsonAsync(
@@ -101,7 +129,7 @@
             } catch (Exception e)
             {
                 stats.IncFailed();
-                var errorMessage = e.Message.Replace("\"", "\\\"");
+                var errorMessage = e.Message.Replace("\\", "\\\\").Replace("\"", "\\\"");
                 return "{" + string.Format("\"errorId\":1,\"errorCode\":\"{0}\", \"errorDescription\":\"CreateTask failed with error: {1}\"",
                     "ERROR_CAPSOLVER_PROXY_EXCEPTION", errorMessage) + "}";
             }
--- CapSolverProxy/CapSolverSettings.cs
+++ CapSolverProxy/CapSolverSettings.cs
@@ -5,5 +5,7 @@
         public int CacheSizeLimit { get; set; }
         public int CacheSlidingExpiration { get; set; }
         public int CacheAbsoluteExpiration { get; set; }
+        public string? ImagesFolder { get; set; }
+        public bool SaveImages { get; set; }
     }
 }
