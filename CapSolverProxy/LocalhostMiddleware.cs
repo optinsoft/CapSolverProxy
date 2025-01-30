410025a043b66388c0ab62f8e8bac53f0ba596fd
vitaly <optinsoft@gmail.com>
2025-01-30 20:30:30 +0500

    allow use from local IP only

---
+++ CapSolverProxy/LocalhostMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+
+namespace CapSolverProxy
+{
+    public class LocalhostMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public LocalhostMiddleware(RequestDelegate next)
+        {
+            _next = next;
+
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var remoteIpAddress = context.Connection.RemoteIpAddress;
+            if (remoteIpAddress == null || !IPAddress.IsLoopback(remoteIpAddress))
+            {
+                context.Response.StatusCode = StatusCodes.Status403Forbidden;
+                await context.Response.WriteAsync("Forbidden.");
+                return;
+            }
+
+            await _next(context);
+        }
+    }
+
+}
--- CapSolverProxy/Program.cs
+++ CapSolverProxy/Program.cs
@@ -7,6 +7,8 @@ using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.UseMiddleware<LocalhostMiddleware>();
+
 var settings = builder.Configuration.GetSection("CapSolverProxy").Get<CapSolverSettings>();
 
 using ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole());
